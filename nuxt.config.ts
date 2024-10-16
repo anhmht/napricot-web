0e73c95a073dbf5680515e1d5418e4f23f192301
Mai Hoàng Trí Anh <anhmht@gmail.com>
2024-10-16 13:52:36 +0700

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,10 +73,6 @@ export default defineNuxtConfig({
     '~/assets/styles/main.css'
   ],
 
-  device: {
-    defaultUserAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1',
-  },
-
   modules: [
     '@pinia/nuxt',
     "@nuxt/image",
