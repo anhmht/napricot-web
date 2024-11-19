15d9af9ea7cab7523cd5ef21b71cd4c9c946e485
Mai Hoàng Trí Anh <anhmht@gmail.com>
2024-11-20 01:32:41 +0700

    improve font preload onload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { RuntimeConfig, getRunTimeConfig } from "./config/RuntimeConfig"
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 const TARGET_ENV = process.env.TARGET_ENV ?? 'staging'
+import fontsPreload from "./config/Font"
 
 const runtimeConfig: RuntimeConfig = getRunTimeConfig(TARGET_ENV)
 
@@ -39,6 +40,7 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'Napricot is the best place to find gifts for your loved ones. We have a wide range of gifts for all occasions.' }
       ],
+      link: [...fontsPreload]
     },
   },
 
